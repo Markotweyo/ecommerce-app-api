e025b07bde3c41b29375b7ff457fd64b6ce06ef5
Mark Otweyo <markotweyo99@gmail.com>
2021-10-29 15:26:20 +0300

    Added Product routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express ()
 const dotenv= require('dotenv')
 const userRoute= require('./routes/user')
 const authRoute= require('./routes/auth')
+const productRoute= require('./routes/product')
 
 dotenv.config()
 
@@ -16,7 +17,8 @@ mongoose.connect( process.env.MONGO_URL )
 
 app.use(express.json())
 app.use('/api/users', userRoute);
-app.use('/api/auth', authRoute, )
+app.use('/api/auth', authRoute )
+app.use('/api/products', productRoute, )
 
 
 app.listen(process.env.PORT || 5000, ()=>{
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -20,7 +20,7 @@ const verifyToken = (req, res, next)=>{
 const verifyTokenAndAuthorization= (req, res, next)=>{
     verifyToken(req, res, ()=>{
         if (req.user.id===req.params.id || req.user.isAdmin){
-            next()
+            next();
         }else {
             return res.status(403).json("You are not allowed to do that!")
         }
